897190b5a0f2a143ecff1931c9abfa58bd8714ad
Marvin Garcia <magar@microsoft.com>
2022-06-07 12:57:27 -0400

    added both car and user rest apis

---
+++ ingress-nginx/CarApi/CarApi/Car.cs
@@ -0,0 +1,22 @@
+﻿namespace CarApi
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public Car(int id, string make, string model, int year, string color)
+        {
+            this.Id = id;
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+        }
+
+        public Car() { }
+    }
+}
