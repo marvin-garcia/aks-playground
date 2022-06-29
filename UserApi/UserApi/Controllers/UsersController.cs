0b383f939e0ff69dc4adac4c04891081fbc8457a
Marvin Garcia <magar@microsoft.com>
2022-06-29 13:40:41 -0400

    changed api routes

--- UserApi/UserApi/Controllers/UsersController.cs
+++ UserApi/UserApi/Controllers/UsersController.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace UserApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UsersController : Controller
     {
