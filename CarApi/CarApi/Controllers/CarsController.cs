fc7a53319fea40d409c8d986c2a5046ec9f02ad6
Marvin Garcia <magar@microsoft.com>
2022-06-29 13:40:41 -0400

    changed api routes

--- CarApi/CarApi/Controllers/CarsController.cs
+++ CarApi/CarApi/Controllers/CarsController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CarApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class CarsController : Controller
     {
--- ConsoleApi/ConsoleApi/main.py
+++ ConsoleApi/ConsoleApi/main.py
@@ -28,20 +28,11 @@ app = FastAPI()
 def get_docs():
     return RedirectResponse("/docs")
 
-@app.get("/api/consoles")
+@app.get("/consoles")
 def get_consoles():
     return consoles
 
-@app.get("/api/consoles/{id}")
+@app.get("/consoles/{id}")
 def get_console(id: int):
     console = list(filter(lambda x: x['id'] == id, consoles))[0]
     return console
-
-@app.get("/api/consolesbyid")
-def get_console_query(id: int):
-    console = list(filter(lambda x: x['id'] == id, consoles))[0]
-    return console
-
-@app.get("/api/multiply/{a}/{b}")
-def multiply(a: float, b: float):
-    return a * b
