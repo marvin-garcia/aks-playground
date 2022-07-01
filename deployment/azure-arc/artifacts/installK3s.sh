81226805ba08ce0e91448c53b5ceab640cf0d9a9
Marvin Garcia <magar@microsoft.com>
2022-07-01 18:25:50 -0400

    commented out setting public IP for cluster

--- deployment/azure-arc/artifacts/installK3s.sh
+++ deployment/azure-arc/artifacts/installK3s.sh
@@ -45,8 +45,8 @@ while sleep 1; do sudo -s rsync -a /var/lib/waagent/custom-script/download/0/ins
 sudo -u $adminUsername mkdir /home/${adminUsername}/.kube
 curl -sLS https://get.k3sup.dev | sh
 # sudo cp k3sup /usr/local/bin/k3sup
-sudo k3sup install --local --user $adminUsername --context $vmName --ip $publicIp --k3s-extra-args '--no-deploy traefik'
-# sudo k3sup install --local --context arcbox-k3s --k3s-extra-args '--no-deploy traefik'
+# sudo k3sup install --local --user $adminUsername --context $vmName --ip $publicIp --k3s-extra-args '--no-deploy traefik'
+sudo k3sup install --local --context $vmName --k3s-extra-args '--no-deploy traefik'
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 sudo cp kubeconfig /home/${adminUsername}/.kube/config
 sudo cp kubeconfig /home/${adminUsername}/.kube/config.staging
