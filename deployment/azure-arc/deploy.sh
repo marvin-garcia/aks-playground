65c160bfe468718cf35124f04b70d77611da15d8
Marvin Garcia <magar@microsoft.com>
2022-07-05 21:58:54 -0400

    added automation script

---
+++ deployment/azure-arc/deploy.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+clear
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHTGREEN='\033[1;32m'
+
+printf "${LIGHTGREEN}Type the Azure region you wish to use (no spaces and all lowercase. i.e., 'westcentralus'):${NC}"
+read location
+
+printf "${LIGHTGREEN}How many Rancher k3s cluster VMs do you want to create?${NC}"
+read vmCount
+
+unique=$(echo $RANDOM | md5sum | head -c 8)
+resourceGroupName="arc-k8s-$unique"
+vmNamePrefix="k3s-$unique"
+workspaceName="workspace-$unique"
+addressPrefix="172.16.0.0/16"
+arcSubnetAddressPrefix="172.16.1.0/24"
+bastionSubnetAddressPrefix="172.16.3.64/26"
+
+repoUrl="https://github.com/marvin-garcia/aks-playground"
+repoBranch=$(git rev-parse --abbrev-ref HEAD)
+
+account=$(az account show -o tsv --query "[tenantId, id]")
+tenantId=$(echo $account | awk '{print $1;}')
+subscriptionId=$(echo $account | awk '{print $2;}')
+
+# Create resource group
+az group create \
+    -n $resourceGroupName \
+    -l $location
+
+# Create service principal
+spnName=="arc-k8s-$unique"
+az ad sp create-for-rbac -n $spnName --role "Contributor" --scopes /subscriptions/$subscriptionId
+az ad sp create-for-rbac -n $spnName --role "Security admin" --scopes /subscriptions/$subscriptionId
+spn=$(az ad sp create-for-rbac -n $spnName --role "Security reader" --scopes /subscriptions/$subscriptionId -o tsv --query "[appId, password]")
+spnClientId=$(echo $spn | awk '{print $1;}')
+spnPassword=$(echo $spn | awk '{print $2;}')
+
+# Deploy resources
+deploymentName="arc-k8s-$unique"
+echo -e "Creating deployment $deploymentName"
+
+output=$(az deployment group create \
+    -g $resourceGroupName \
+    --mode Incremental \
+    --name $deploymentName \
+    --template-file ./deployment/azure-arc/bicep/azuredeploy.bicep \
+    --parameters location=$location \
+    --parameters sshRSAPublicKey=@~/.ssh/id_rsa.pub \
+    --parameters vmCount=$vmCount \
+    --parameters vmNamePrefix=$vmNamePrefix \
+    --parameters addressPrefix=$addressPrefix \
+    --parameters arcSubnetAddressPrefix=$arcSubnetAddressPrefix \
+    --parameters spnClientId=$spnClientId \
+    --parameters spnClientSecret=$spnPassword \
+    --parameters spnTenantId=$tenantId \
+    --parameters logAnalyticsWorkspaceName=$workspaceName \
+    --parameters githubBranch=$repoBranch \
+    --query 'properties.outputs' -o json)
+
+if [[ $? -gt 0 ]]
+then
+    exit 1
+fi
+
+# Install jq
+sudo apt install -y jq
+
+for (( c=0; c<$vmCount; c++))
+do
+    vm=$(echo $output | jq -r ".arcBox.value[$c]")
+    vmName=$(echo $vm | jq -r ".vmName")
+    ipAddress=$(echo $vm | jq -r ".privateIpAddress")
+
+    rm -rf "./infrastructure/$vmName"
+
+    echo -e "\n$(tput setaf 3)Creating cluster infrastructure folder for '$vmName'\n$(tput setaf 7)"
+
+    mkdir -p "./infrastructure/$vmName"
+    
+    echo -e "\n$(tput setaf 3)Writing cluster's kustomization file for '$vmName'\n$(tput setaf 7)"
+    
+    cat << EOF > "./infrastructure/$vmName/kustomization.yaml"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - ../ingress-nginx
+patchesStrategicMerge:
+  - values.yaml
+EOF
+
+    echo -e "\n$(tput setaf 3)Writing cluster's values file for '$vmName'\n$(tput setaf 7)"
+    
+    cat << EOF > "./infrastructure/$vmName/values.yaml"
+apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: ingress-nginx
+  namespace: cluster-config
+spec:
+  chart:
+    spec:
+      chart: ingress-nginx
+      sourceRef:
+        kind: HelmRepository
+        name: ingress-nginx
+        namespace: cluster-config
+  values:
+    controller:
+      service:
+        type: LoadBalancer
+        externalIPs:
+        - '$ipAddress'
+EOF
+
+    echo -e "\n$(tput setaf 3)Pushing infrastructure files to repo for '$vmName'\n$(tput setaf 7)"
+
+    git add "./infrastructure/$vmName"
+    git commit -m "Added infrastructure files for '$vmName'"
+    git push
+
+    if [[ $? -gt 0 ]]
+    then
+        exit 1
+    fi
+
+    az k8s-configuration flux create \
+        -g $resourceGroupName \
+        -c $vmName \
+        -n sources \
+        -t connectedClusters \
+        --namespace cluster-config \
+        --scope cluster \
+        -u $repoUrl \
+        --branch $repoBranch \
+        --kustomization name=infra path=./infrastructure/sources prune=true
+
+    if [[ $? -gt 0 ]]
+    then
+        exit 1
+    fi
+
+    az k8s-configuration flux create \
+        -g $resourceGroupName \
+        -c $vmName \
+        -n cluster-config \
+        -t connectedClusters \
+        -u $repoUrl \
+        --branch $repoBranch \
+        --kustomization name=infra path=./infrastructure/$vmName prune=true \
+        --namespace cluster-config \
+        --scope cluster
+
+    if [[ $? -gt 0 ]]
+    then
+        exit 1
+    fi
+
+    az k8s-configuration flux create \
+        -g $resourceGroupName \
+        -c $vmName \
+        -n apps-config \
+        -t connectedClusters \
+        -u $repoUrl \
+        --branch $repoBranch \
+        --kustomization name=apps path=./apps prune=true \
+        --namespace cluster-config \
+        --scope cluster
+
+    if [[ $? -gt 0 ]]
+    then
+        exit 1
+    fi
+done
+
+printf "${LIGHTGREEN}Deployment finished successfully.\n"
+printf "${GREEN}Resource Group name: $resourceGroupName\n\n"
+exit 0
