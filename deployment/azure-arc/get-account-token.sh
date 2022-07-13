c11a91d6fcbb0d4da89729bd1829e89fbdc0987f
Marvin Garcia <magar@microsoft.com>
2022-07-13 14:46:54 -0400

    updated connect script

--- deployment/azure-arc/get-account-token.sh
+++ deployment/azure-arc/get-account-token.sh
@@ -1,3 +1,6 @@
+CLUSTER_NAME='k3s-95eada54-1'
+RESOURCE_GROUP='arc-k8s-0713220958-95eada54'
+
 kubectl create serviceaccount azureportal-user
 kubectl create clusterrolebinding azureportal-user-binding --clusterrole cluster-admin --serviceaccount default:azureportal-user
 kubectl apply -f - <<EOF
@@ -11,4 +14,6 @@ type: kubernetes.io/service-account-token
 EOF
 
 TOKEN=$(kubectl get secret azureportal-user-secret -o jsonpath='{$.data.token}' | base64 -d | sed $'s/$/\\\n/g')
-echo $TOKEN
+echo -e "$TOKEN\n\n"
+
+az connectedk8s proxy -g $RESOURCE_GROUP -n $CLUSTER_NAME --token $TOKEN
